b831b5b46a1989b2f1b4d58b4be573eafe3292c0
Archana Paithankar <paithankararchana992@gmail.com>
2026-06-08 19:13:33 +0530

    changed repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/archana_paithankar_portfolio-/',
+    base: '/archana_portfolio_1.0/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
